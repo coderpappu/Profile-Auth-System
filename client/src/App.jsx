f995c33e8d48339660cce37c4d846cf56c422715
pappudey <pappudey.coder@gmail.com>
2023-06-28 02:23:39 +0600

    Profiel System Client Site ready

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,54 @@
-import './App.css'
+import React from 'react';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-function App() {
+// import all components
+import PageNotFound from "./components/PageNotFound";
+import Password from "./components/Password";
+import Profile from "./components/Profile";
+import Recovery from "./components/Recovery";
+import Register from "./components/Register";
+import Reset from "./components/Reset";
+import Username from "./components/Username";
 
 
+// root routes 
+const router = createBrowserRouter([
+    {
+        path : "/",
+        element : <Username></Username>
+    },
+    {
+        path : '/register',
+        element : <Register></Register>
+    },
+    {
+        path : '/password',
+        element : <Password></Password>
+    },
+    {
+        path : '/profile',
+        element : <Profile></Profile>
+    },
+    {
+        path : '/recovery',
+        element : <Recovery></Recovery>
+    },
+    {
+        path : '/reset',
+        element : <Reset></Reset>
+    },
+    {
+        path : '/*',
+        element : <PageNotFound></PageNotFound>
+    },
+
+])
+
+const App = () => {
   return (
-    <>
-     <h1> Profile CArd App</h1>
-    </>
+    <main>
+        <RouterProvider router={router}></RouterProvider>
+    </main>
   )
 }
 
